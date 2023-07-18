ab071861ab61370a85188c05d7b8c85c96251a03
ryo ichinose <ryo.ichinose@recochoku.co.jp>
2023-07-19 00:47:26 +0900

    楽曲検索〜楽曲再生までの実装

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,68 +1,278 @@
 "use client";
 import React from "react";
-import { IconButton, Stack, Box, Text, Image, Input } from "@chakra-ui/react";
+import {
+  IconButton,
+  Stack,
+  Box,
+  Text,
+  Image,
+  Input,
+  HStack,
+} from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import axios from "axios";
+import ReactAudioPlayer from "react-audio-player";
 
-function SearchBarAndIconFrame() {
+interface Images {
+  url: string;
+  height: number;
+  width: number;
+}
+
+interface Music {
+  id: number | string;
+  song_title: string;
+  artist_name: string;
+  song_url: string;
+  album_name: string;
+  images: Images;
+}
+
+const textStyle: React.CSSProperties = {
+  fontFamily: "Raleway",
+  fontWeight: "medium",
+  wordBreak: "break-all",
+};
+
+const mainFrameProps = {
+  justify: "flex-start",
+  align: "flex-start",
+  spacing: "10px",
+  width: "813px",
+  height: "641px",
+  maxWidth: "100%",
+};
+
+// APIを呼び出して検索結果を返す関数
+async function searchMusic(query: string): Promise<Music[]> {
+  try {
+    const searchQueryUri = `http://localhost:8000/songs?search_words=${query}`;
+    const response = await axios.get<Music[]>(searchQueryUri);
+
+    console.log(response.data);
+    for (let i = 0; i < response.data.length; i++) {
+      console.log(response.data[i].id);
+      console.log(response.data[i].song_title);
+      console.log(response.data[i].song_url);
+    }
+    return response.data;
+  } catch (error) {
+    // Axios error handling
+    if (axios.isAxiosError(error)) {
+      throw new Error(`APIの呼び出しでエラーが発生しました。 ${error.message}`);
+    }
+
+    // Network error or other unexpected errors
+    throw new Error("予期しないエラーが発生しました。");
+  }
+}
+
+function SearchMusicComponent() {
+  const [query, setQuery] = useState<string>("");
+  // 楽曲データの検索結果を受け取るuseState.
+  const [musicData, setMusicData] = useState<Music[]>([]);
+
+  const handleSearch = async () => {
+    try {
+      setMusicData([]); // 検索結果をリセット
+      const results = await searchMusic(query);
+      setMusicData(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <Stack
-      direction="row"
-      justify="flex-start"
-      align="flex-start"
-      spacing="-8px"
-      width="1152px"
-      maxWidth="100%"
-    >
-      <Stack justify="flex-start" align="flex-start" spacing="10px">
-        <Box
-          width="813px"
-          height="641px"
-          maxWidth="100%"
-          background="rgba(222, 207, 131, 0.27)"
-          position="relative"
-        />
-        <Text
-          fontFamily="PT Mono"
-          lineHeight="0.94"
-          fontWeight="regular"
-          fontSize="25px"
-          letterSpacing="0.04em"
-          color="#000000"
-          width="42px"
-          height="20px"
-          top="12px"
-          left="20px"
-          textAlign="center"
-          position="absolute"
-        >
-          Mu si ca
-        </Text>
-        <Input
-          width="572px"
-          height="47px"
-          maxWidth="100%"
-          top="22px"
-          left="92px"
-          background="#FFFFFF"
-          position="absolute"
-          type="text"
-          placeholder=" Search..."
-        />
-        <IconButton
-          aria-label="Search"
-          width="27px"
-          height="27px"
-          left="614px"
-          top="32px"
-          icon={<SearchIcon />}
-          position="absolute"
-        />
-      </Stack>
+    <Stack>
+      <IconComponent />
+      <SearchBarComponent
+        onSearch={handleSearch}
+        query={query}
+        setQuery={setQuery}
+      />
+      <SearchResultComponent musicData={musicData} />
     </Stack>
   );
 }
 
-function RadioFrame() {
+// 子コンポーネント1: SearchBarComponent
+interface SearchBarProps {
+  onSearch: () => void;
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchBarComponent: React.FC<SearchBarProps> = ({
+  onSearch,
+  query,
+  setQuery,
+}) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <>
+      <Input
+        width="572px"
+        height="47px"
+        maxWidth="100%"
+        top="22px"
+        left="92px"
+        background="#FFFFFF"
+        position="absolute"
+        type="text"
+        placeholder=" Search..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <IconButton
+        aria-label="Search"
+        width="27px"
+        height="27px"
+        left="614px"
+        top="32px"
+        icon={<SearchIcon />}
+        position="absolute"
+        onClick={onSearch}
+      />
+    </>
+  );
+};
+
+// 子コンポーネント2: SearchResultComponent
+interface SearchResultProps {
+  musicData: Music[];
+}
+
+// async function playMusic(currentSongId: string): Promise<any> {
+//   try {
+//     const playMusicUri = `http://localhost:8000/songs/${currentSongId}`;
+//     await axios.post<any>(playMusicUri);
+//     // return success
+//   } catch (error) {
+//     // Axios error handling
+//     if (axios.isAxiosError(error)) {
+//       throw new Error(`APIの呼び出しでエラーが発生しました。 ${error.message}`);
+//     }
+
+//     // Network error or other unexpected errors
+//     throw new Error("予期しないエラーが発生しました。");
+//   }
+// }
+
+const SearchResultComponent: React.FC<SearchResultProps> = ({ musicData }) => {
+  // const handlePlayMusic = (songId: any) => {
+  //   playMusic(songId);
+  // };
+
+  return (
+    <>
+      <Stack>
+        <Text
+          fontFamily="Raleway"
+          fontWeight="bold"
+          fontSize="17px"
+          color="#000000"
+          width="197px"
+          height="24px"
+          left="20px"
+          top="330px"
+          position="absolute"
+        >
+          Search Result
+        </Text>
+        <HStack
+          left="18px"
+          top="364px"
+          position="absolute"
+          style={{ flexWrap: "wrap" }}
+        >
+          {musicData.map((music) => (
+            <Box
+              key={music.id}
+              width="125px"
+              height="88px"
+              style={{ margin: "30px 0" }}
+            >
+              <Image width="100px" height="100px" src={music.images.url} />
+              <ReactAudioPlayer
+                style={{ maxWidth: "105px", height: "8px" }}
+                src={music.song_url}
+                // onPlay={() => handlePlayMusic(music.id)}
+                controls
+              />
+              <Text fontSize="10px" color="#000000" style={textStyle}>
+                {music.song_title}
+              </Text>
+              <Text
+                fontSize="8px"
+                color="rgba(0, 0, 0, 0.32)"
+                style={textStyle}
+              >
+                {music.artist_name}
+              </Text>
+            </Box>
+          ))}
+        </HStack>
+      </Stack>
+    </>
+  );
+};
+
+function IconComponent() {
+  return (
+    <>
+      <Text
+        fontFamily="PT Mono"
+        lineHeight="0.94"
+        fontWeight="regular"
+        fontSize="25px"
+        letterSpacing="0.04em"
+        color="#000000"
+        width="42px"
+        height="20px"
+        top="12px"
+        left="20px"
+        textAlign="center"
+        position="absolute"
+      >
+        Mu si ca
+      </Text>
+    </>
+  );
+}
+// 画面左側の最も大きい枠. メインフレーム.
+function SearchAndResultFrame() {
+  return (
+    <Box>
+      <Stack
+        direction="row"
+        justify="flex-start"
+        align="flex-start"
+        spacing="-8px"
+        width="1152px"
+        maxWidth="100%"
+      >
+        <Stack
+          {...mainFrameProps}
+          background="rgba(222, 207, 131, 0.27)"
+          position="relative"
+          overflowY="scroll"
+          css={{
+            scrollBehavior: "smooth",
+          }}
+        >
+          <RadioComponent />
+          <SearchMusicComponent />
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
+
+function RadioComponent() {
   return (
     <>
       <Box
@@ -199,16 +409,6 @@ function RadioFrame() {
         alt="Zane Lowe"
         position="absolute"
       />
-    </>
-  );
-}
-
-// 画面左側の最も大きい枠.
-function RecommendFrame() {
-  return (
-    <>
-      <SearchBarAndIconFrame />
-      <RadioFrame />
     </>
   );
 }
@@ -674,7 +874,7 @@ function ReplayFrame() {
 export const SearchPage = () => (
   <>
     <Box>
-      <RecommendFrame />
+      <SearchAndResultFrame />
       <MyFavoriteTrackFrame />
       <ReplayFrame />
     </Box>
